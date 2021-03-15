6f01199144ae01856fd8a7b4624680a9438971e0
Denis Klimenko <jaschik26@yandex.ru>
2021-03-15 22:38:44 +0400

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  
+  str = String(str);
+  let addition = ''
+
+  if (options.addition || String(options.addition) === 'false' || String(options.addition) === 'null') {
+    addition = String(options.addition);
+  }
+  
+  let separator = options.separator || '+';
+  let additionSeparator = options.additionSeparator || '|';
+  let addRepits = options.additionRepeatTimes || 1;
+  let repits = options.repeatTimes || 1;
+
+  let fullAddition = '';
+
+  if (addRepits > 1) {
+    for (let i = 0; i < addRepits; i++) {
+      if (i === (addRepits - 1)) {
+        fullAddition = fullAddition + addition;
+      } else {
+        fullAddition = fullAddition + addition + additionSeparator;
+      };
+    };
+  } else {
+    fullAddition = addition;
+  }
+  
+  let result = '';
+
+  for (let i = 0; i < repits; i++) {
+    if (i === (repits - 1)) {
+      result = result + str + fullAddition;
+    } else {
+      result = result + str + fullAddition + separator;
+    };
+  }
+
+  return result
 };
   
