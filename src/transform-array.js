73c849fd1593760cbfe1ac5697869631ee8f8a4c
Klim <jaschik26@yandex.ru>
2021-03-17 15:59:58 +0400

    feat: implement transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  const _arr = arr.slice();
+
+  const result = [];
+  
+  for (let i = 0; i < _arr.length; i++) {
+
+    switch (_arr[i]) {
+
+      case '--discard-next':
+        if (_arr[i+1] !== undefined) {
+          _arr[i+1] = 'discarded'
+          i++;
+        } else {
+          return result
+        }    
+      break;
+
+      case '--discard-prev':
+        if (_arr[i-1] === 'discarded') {
+          break
+        } else if (_arr[i-1] !== undefined) {
+          result.pop();
+        }
+      break;
+
+      case '--double-next':
+        if (_arr[i+1] !== undefined) {
+          result.push(_arr[i+1]);
+        } else {
+          return result
+        }
+      
+      break;
+
+      case '--double-prev':
+        if (_arr[i-1] === 'discarded') {
+          break
+        } else if (_arr[i-1] !== undefined) {
+          result.push(_arr[i-1]);
+        }
+      break;
+
+      default:
+        result.push(_arr[i])
+    };
+  };
+
+  return result
 };
+
