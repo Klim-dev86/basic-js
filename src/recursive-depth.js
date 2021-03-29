6d3412136d6dc47ca48d3131c487c5ed1feba6f3
Klim <jaschik26@yandex.ru>
2021-03-29 11:49:02 +0400

    feat: finish recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,11 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
 
-
   calculateDepth(arr) {
-    console.log(arr)
     return Array.isArray(arr) ? 
-    1 + Math.max(...arr.map(this.calculateDepth.bind(this))) :
+    1 + Math.max(0, ...arr.map(this.calculateDepth.bind(this))) :
     0;
   }
 };
